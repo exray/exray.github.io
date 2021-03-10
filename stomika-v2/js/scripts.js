0d109a7fe1c3977ebcf8884c7b0aa6ebeaf2add2
exray <41897176+exray@users.noreply.github.com>
2021-03-10 19:35:19 +0300

    Fixes+responsive

--- stomika-v2/js/scripts.js
+++ stomika-v2/js/scripts.js
@@ -1,24 +1,29 @@
-function navSlide() {
-    const burger = document.querySelector(".burger");
-    const nav = document.querySelector(".nav-links");
-    const navLinks = document.querySelectorAll(".nav-links li");
+// function navSlide() {
+//     const burger = document.querySelector(".burger");
+//     const nav = document.querySelector(".nav-links");
+//     const navLinks = document.querySelectorAll(".nav-links li");
     
-    burger.addEventListener("click", () => {
-        //Toggle Nav
-        nav.classList.toggle("nav-active");
+//     burger.addEventListener("click", () => {
+//         //Toggle Nav
+//         nav.classList.toggle("nav-active");
         
-        //Animate Links
-        navLinks.forEach((link, index) => {
-            if (link.style.animation) {
-                link.style.animation = ""
-            } else {
-                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
-            }
-        });
-        //Burger Animation
-        burger.classList.toggle("toggle");
-    });
+//         //Animate Links
+//         navLinks.forEach((link, index) => {
+//             if (link.style.animation) {
+//                 link.style.animation = ""
+//             } else {
+//                 link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.5}s`;
+//             }
+//         });
+//         //Burger Animation
+//         burger.classList.toggle("toggle");
+//     });
     
-}
+// }
 
-navSlide();
+// navSlide();
+
+window.addEventListener("scroll", function(){
+    const menu = document.querySelector(".top-nav-wrapper");
+    menu.classList.toggle("sticky", window.scrollY > 0);
+});
